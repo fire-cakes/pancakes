cb6af549bf972ea3b46486ae9102cd52243dbdb7
mahalocode <you@example.com>
2016-10-31 05:03:58 +0000

    chess board looks great, app function

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,43 +1,10 @@
 class GamesController < ApplicationController
-<<<<<<< HEAD
-	# before_action :authenticate_player!
-	def index
-	end
-
-	def show
-	end
-
-	def new
-  		@available_game = Game.new
-	end
-
-	def create
-		@available_game = Game.new(params[:game])
-	end
-
-	def edit
-	end
-
-	def update
-		@available_game = Game.find(params[:id])
-
-		if @available_game.update_attributes(params[:game])
-			redirect_to root_path, :notice => "New game ready"
-		else
-			render "fail to load new game"
-	end
-
-	def destroy
-		@available_game = Game.find(params[:id])
-		@available_game.destroy
-		redirect_to games_path, :notice => "Your game has been cancel"
-	end
-=======
+  # before_action :authenticate_player!
   def index
   end
 
   def show
-    @available_game = Game.find(params[:id])
+    @available_game = Game.new
   end
 
   def new
@@ -46,7 +13,7 @@ class GamesController < ApplicationController
 
   def create
     @available_game = game_params[:user_id]
-    @available_game = Game.create(user2_id: new_user_id, user_id: current_user.id )
+    @available_game = Game.create(user2_id: new_user_id, user_id: current_user.id)
     @available_game.initialize_game_board!
     redirct_to games_path(@available_game)
   end
@@ -61,17 +28,18 @@ class GamesController < ApplicationController
       redirect_to games_path, notice: 'New game ready'
     else
       render 'fail to load new game'
+    end
   end
 
-    def destroy
-      @available_game = Game.find(params[:id])
-      @available_game.destroy
-      redirect_to games_path, notice: 'Your game has been cancel'
-    end
+  def destroy
+    @available_game = Game.find(params[:id])
+    @available_game.destroy
+    redirect_to games_path, notice: 'Your game has been cancel'
+  end
 
-    private
-    def game_params
-      params.require(:game).permit(:user_id, :user2_id)
->>>>>>> model
-end
+  private
+
+  def game_params
+    params.require(:game).permit(:user_id, :user2_id)
+  end
 end
