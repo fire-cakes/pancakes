4104614bb1e37218ea73502bca1f042636149823
Dan <danielpowell12@msn.com>
2016-11-02 16:46:41 -0600

    Removes odd duplicate class in Application Controller
    Through git blame I was able to see that this code
    was added in bulk around the time omni-auth was being
    fiddled with.
    
    As it was put on hold (or abandoned) I am removinf this
    code for now to be safe.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,34 +3,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
 
-    helper_method :current_user
-    helper_method :user_signed_in?
-    helper_method :correct_user?
-
-    private
-
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    rescue Exception => e
-      nil
-    end
-
-    def user_signed_in?
-      return true if current_user
-    end
-
-    def correct_user?
-      @user = User.find(params[:id])
-      redirect_to root_url, alert: 'Access denied.' unless current_user == @user
-    end
-
-    def authenticate_user!
-      unless current_user
-        redirect_to root_url, alert: 'You need to sign in for access to this page.'
-      end
-    end
-end
 end
