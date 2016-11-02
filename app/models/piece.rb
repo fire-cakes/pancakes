c7a1db3030ca2d3460970a13274effb930714ea3
Dan <danielpowell12@msn.com>
2016-11-02 17:00:07 -0600

    Wrap todo comment text onto several lines

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -82,7 +82,9 @@ class Piece < ActiveRecord::Base
   end
 
   # this method will move a piece to new location and capture if appropriate
-  # note that the piece controller is being built by someone else right now, and updating the piece location will require an update method in that controller so it won't function correctly yet
+  # TODO piece controller is being built by someone else right now,
+  # and updating the piece location will require an update method in that
+  # controller so it won't function correctly yet
   def move_to?(new_x, new_y)
     if pos_filled?(new_x, new_y) == true
       if return_piece(new_x, new_y).user_id != current_user
