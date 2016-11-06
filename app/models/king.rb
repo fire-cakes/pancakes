406d45dc055322632ac2f918b55bb537af53235d
Dan <danielpowell12@msn.com>
2016-11-05 21:25:35 -0600

    Adds HTML piece characters to models
    Sourced from https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,6 @@
 # frozen_string_literal: true
-class King < Piece; end
+class King < Piece
+  def image
+    color ? '&#9812;' : '&#9818;'
+  end
+end
--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,6 @@
 # frozen_string_literal: true
-class Queen < Piece; end
+class Queen < Piece
+  def image
+    color ? '&#9813;' : '&#9819;'
+  end
+end
