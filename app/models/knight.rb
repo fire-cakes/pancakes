193d4de8db79978d4283ae3c7d11beddc475ffe8
icrustandi <icrustandi@gmail.com>
2016-11-09 20:13:39 +0000

    TDD knight valid_move?

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,4 +3,14 @@ class Knight < Piece
   def image
     color ? '&#9816;' : '&#9822;'
   end
+
+  def valid_move?(x1, y1)
+    return false if x_coord == x1
+    return false if y_coord == y1
+    correct_move?(x1, y1)
+  end
+
+  def correct_move?(x1, y1)
+    (x_coord - x1).abs + (y_coord - y1).abs == 3
+  end
 end
