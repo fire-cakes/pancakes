7cb87bc45703404769df4b47ae8a6954a967133d
Dan <danielpowell12@msn.com>
2016-11-11 17:49:22 -0700

    Auto fixes for rubocop

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
 FactoryGirl.define do
-
-factory :player do
+  factory :player do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
     end
-    password "secretPassword"
-    password_confirmation "secretPassword"
-
+    password 'secretPassword'
+    password_confirmation 'secretPassword'
   end
 end
