1ba6713594d4edded645f2cf910e9a2e1c314872
Dan <danielpowell12@msn.com>
2016-11-26 13:45:48 -0700

    Add route to update pieces

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :players
   root 'games#index'
   resources :games
+  resources :pieces, only: :update
 end
 
 # The priority is based upon order of creation: first created -> highest priority.
