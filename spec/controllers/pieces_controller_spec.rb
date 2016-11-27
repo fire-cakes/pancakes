03b42a559d4dd320871bd59f1e43edbb08daf24d
mahalocode <you@example.com>
2016-11-27 00:40:01 +0000

    finish move_to

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -2,4 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
+  describe 'pieces#update action' do
+    it 'successfully update the page' do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
