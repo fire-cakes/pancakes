9c03e3870ed2535a7213e2bb14cfe44944f820bc
icrustandi <icrustandi@gmail.com>
2016-11-28 07:38:01 +0000

    Use populate_board trait in factory for tests

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -24,8 +24,7 @@ RSpec.describe Piece, type: :model do
 
   context 'pos_filled?' do
     it 'should return true if there is a piece at the inputted location' do
-      g = FactoryGirl.create(:game, :with_two_players)
-      g.populate_board!
+      g = FactoryGirl.create(:game, :with_two_players, :populate_board)
       game_id = g.id
       piece = g.pieces.first
 
@@ -52,23 +51,20 @@ RSpec.describe Piece, type: :model do
       expect(piece.black?).to be false
     end
     it 'right_color? should return true for black piece on black turns' do
-      game = FactoryGirl.create(:game, :with_two_players)
-      game.populate_board!
+      game = FactoryGirl.create(:game, :with_two_players, :populate_board)
       game.increment_turn
       piece = game.pieces.where(color: false).last
 
       expect(piece.right_color?).to be true
     end
     it 'right_color? should return true for white piece on white turns' do
-      game = FactoryGirl.create(:game, :with_two_players)
-      game.populate_board!
+      game = FactoryGirl.create(:game, :with_two_players, :populate_board)
       piece = game.pieces.where(color: true).last
 
       expect(piece.right_color?).to be true
     end
     it 'right_color? should return false for pieces moving when not turn' do
-      game = FactoryGirl.create(:game, :with_two_players)
-      game.populate_board!
+      game = FactoryGirl.create(:game, :with_two_players, :populate_board)
       game.increment_turn
       piece = game.pieces.where(color: true).last
 
@@ -78,8 +74,7 @@ RSpec.describe Piece, type: :model do
 
   context 'occupying_piece' do
     it 'should return the piece at the given location' do
-      g = FactoryGirl.create(:game, :with_two_players)
-      g.populate_board!
+      g = FactoryGirl.create(:game, :with_two_players, :populate_board)
       piece = g.pieces.first
 
       expect(piece.occupying_piece(3, 7)).to eq(Piece.where(x_coord: 3, y_coord: 7).first)
