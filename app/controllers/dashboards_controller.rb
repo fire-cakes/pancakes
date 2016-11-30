d11afe4422fe1bc0e6d31511b7014c793bb77e89
mahalocode <you@example.com>
2016-11-30 01:39:11 +0000

    add dashboard controller/route

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,13 +3,13 @@ class DashboardsController < ApplicationController
   before_action :authenticate_player!
 
   def show
-    @feature_games = feature_games
+    @user_games = user_games
     @available_games = available_games
   end
 
-  def feature_games
+  def user_games
     if signed_in?
-      @feature_games = Game.where('white_player_id = ? or black_player_id = ?', current_player.id, current_player.id).order('updated_at').to_a.first(10)
+      @user_games = Game.where(black_player_id: current_player.id) + Game.where(white_player_id: current_player.id).order('updated_at').to_a.first(10)
     end
   end
 
