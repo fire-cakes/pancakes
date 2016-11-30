0c962f1f2e5ae598e8ef0d7a98254769e444fc95
mahalocode <you@example.com>
2016-11-30 03:17:28 +0000

    fixed travis ci complaint

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120201114) do
-
 ActiveRecord::Schema.define(version: 20161127032058) do
 
   # These are extensions that must be enabled in order to support this database
