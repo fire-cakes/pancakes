fbc56802fd9e7e7d77fd769fbe35e29b5aad53e7
mahalocode <you@example.com>
2016-11-30 03:24:57 +0000

    recommit

---
+++ spec/application_helper.rb
@@ -0,0 +1,18 @@
+def flash_message(type, text)
+  flash[type] ||= []
+  flash[type] << text
+end
+
+def render_flash
+  flash_array = []
+  flash.each do |type, messages|
+    messages.each do |m|
+      unless m.blank?
+        flash_array << render(partial: 'shared/flash',
+                              locals: { type: type, message: m })
+      end
+    end
+  end
+
+  flash_array.join('').safe_join
+end
