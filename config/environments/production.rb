8d4b4239b1c6ebfc96cea8e470f6cb3f33d3f362
Dan <danielpowell12@msn.com>
2016-12-04 12:03:13 -0700

    Rubocop fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,6 @@ Rails.application.configure do
 
   # Do fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  
 
   # Generate digests for assets URLs.
   config.assets.digest = true
